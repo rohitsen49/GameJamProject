a425d4f90b6b70f4b80d495194140cbc0b59a766
Shoavel <90283548+RyanMckeich@users.noreply.github.com>
2022-03-02 14:51:52 -0800

    text generator

--- Assets/Scripts/UI_Assistant.cs
+++ Assets/Scripts/UI_Assistant.cs
@@ -5,10 +5,16 @@ using UnityEngine.UI;
 
 public class UI_Assistant : MonoBehaviour
 {
+    [SerializeField] private TextWriter textWriter;
     private Text messageText;
 
     private void Awake()
     {
-        
+        messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+        textWriter.AddWriter(messageText, "Hello World!", 1f);
     }
 }
