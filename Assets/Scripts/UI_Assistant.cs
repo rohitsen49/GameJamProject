a10ad224c4e0b8c4362e88d7883c0860e57ee6db
rohitsen49 <90069220+rohitsen49@users.noreply.github.com>
2022-03-02 13:24:30 -0800

    Added Text template

---
+++ Assets/Scripts/UI_Assistant.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Assistant : MonoBehaviour
+{
+    private Text messageText;
+
+    private void Awake()
+    {
+        
+    }
+}
