de2417ce90bc3c3c01447ff8961e62f95354b675
rohitsen49 <90069220+rohitsen49@users.noreply.github.com>
2022-03-06 00:00:22 -0800

    Finished Settings

--- Assets/Scripts/ChangeSceneWithButton.cs
+++ Assets/Scripts/ChangeSceneWithButton.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneWithButton : MonoBehaviour
 {
+    public bool gamePaused = false;
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+
+        /* if(Input.GetKeyDown(KeyCode.P))
+        {
+            SceneManager.LoadScene("PauseMenu");
+            gamePaused = true;
+
+            Input.GetKeyDown(KeyCode.Escape);
+            {
+                SceneManager.LoadScene(sceneName);
+                gamePaused = false;
+            }
+        } */
     }
 }
