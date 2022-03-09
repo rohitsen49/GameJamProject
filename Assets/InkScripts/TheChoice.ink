5948552aba05dc148d3f939a63eb3bc06cc5aee0
Shoavel <90283548+RyanMckeich@users.noreply.github.com>
2022-03-09 14:51:16 -0800

    AHAHAHAHAHAHA

---
+++ Assets/InkScripts/TheChoice.ink
@@ -0,0 +1,12 @@
+-> main
+
+=== main ===
+Who will you choose?
+   + [Eshu]
+     -> chosen("Eshu")
+   + [Astrill]
+     -> chosen("Astrill")
+     
+=== chosen(date) ===
+You decide to go see {date}!
+-> END
--- Assets/Scripts/ScriptReader.cs
+++ Assets/Scripts/ScriptReader.cs
@@ -11,10 +11,12 @@ public class ScriptReader : MonoBehaviour
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI displayNameText;
+    [SerializeField] private Animator portraitAnimator;
 
     [Header("Choices UI")]
     [SerializeField] private GameObject[] choices;
-    private TextMeshProUGUI[] choiceText;
+    private TextMeshProUGUI[] choicesText;
 
     private Story currentStory;
 
@@ -23,6 +25,10 @@ public class ScriptReader : MonoBehaviour
     public bool panelActivate = true;
 
     private static ScriptReader instance;
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+    private const string LAYOUT_TAG = "layout";
 
     private void Awake()
     {
@@ -44,11 +50,11 @@ public class ScriptReader : MonoBehaviour
         dialoguePanel.SetActive(panelActivate);
 
         //get all of the choices text
-        choiceText = new TextMeshProUGUI[choices.Length];
+        choicesText = new TextMeshProUGUI[choices.Length];
         int index = 0;
         foreach (GameObject choice in choices)
         {
-            choiceText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
             index++;
         }
 
@@ -63,7 +69,7 @@ public class ScriptReader : MonoBehaviour
         }
 
         // handle continuing to the next line in the dialogue when submit is pressed
-        if (InputManager.GetInstance().GetSubmitPressed())
+        if (currentStory.currentChoices.Count == 0 && InputManager.GetInstance().GetSubmitPressed())
         {
             ContinueStory();
         }
@@ -89,8 +95,12 @@ public class ScriptReader : MonoBehaviour
     {
         if (currentStory.canContinue)
         {
+            //set text for current dialogue line
             dialogueText.text = currentStory.Continue();
+            // display choices, if any for this dialogue line
             DisplayChoices();
+            // handle tags
+            HandleTags(currentStory.currentTags);
         }
         else
         {
@@ -98,26 +108,74 @@ public class ScriptReader : MonoBehaviour
         }
     }
 
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            //parse the tag
+            string[] splitTag = tag.Split(':');
+            if(splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be appropriately parsed: " + tag);
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    displayNameText.text = tagValue;
+                    break;
+                case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    break;
+                case LAYOUT_TAG:
+                    Debug.Log("layout=" + tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+                    break;
+            }
+        }
+    }
+
     private void DisplayChoices()
     {
-        //List<Choice> currentChoices = currentStory.currentChoices;
+        List<Choice> currentChoices = currentStory.currentChoices;
 
-        //if (currentChoices.Count > choices.Length)
-        //{
-            //Debug.LogError("More choices were given than the UI can supporyt. Number of choices given: " + currentChoices.Count);
-        //}
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than the UI can supporyt. Number of choices given: " + currentChoices.Count);
+        }
 
-        //int index = 0;
+        int index = 0;
 
-        //foreach(Choice choice in currentChoices)
-        //{
-            //choice[index].gameObject.SetActive(true);
-            //choiceText[index].text = choice.text;
-            //index++;
-        //}
-        //for (int i = index; i < choices.Length; i++)
-        //{
-            //choices[i].gameObject.SetActive(false);
-        //}
+        foreach(Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        // even system requires we clear it first
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+        InputManager.GetInstance().RegisterSubmitPressed();
+        ContinueStory();
     }
 }
